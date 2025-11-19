5745cc494eec0a1fe01784e20134b6ad728b9e99
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-11-19 20:31:34 +0000

    Add backend

---
+++ API/Program.cs
@@ -0,0 +1,40 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//TODO: Enable OpenAPI later
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//TODO: Enable OpenAPI later
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//TODO: Not in use
+// app.UseHttpsRedirection();
+
+//TODO: Enable Authorization later
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
