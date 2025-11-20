297592bd9ddd9b3cf7309778aa30035d384b0fcd
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-11-20 16:31:13 +0000

    add authentication

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,16 +18,27 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddCors();
-
-//TODO: Enable OpenAPI later
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"]
+            ?? throw new Exception("Token key not found");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
 
 var app = builder.Build();
 
-//TODO: Enable OpenAPI later
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -42,6 +58,10 @@ app.UseCors(policy =>
     policy.AllowAnyMethod();
     policy.WithOrigins("http://localhost:4200", "https://localhost:4200");
 });
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
